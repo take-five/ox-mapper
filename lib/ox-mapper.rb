e7c25dff244992e40f0cb182b75d8174a6718972
Alexei Mikhailov <amikhailov83@gmail.com>
2013-08-15 14:58:53 +0600

    Fix inline documentation

--- lib/ox-mapper.rb
+++ lib/ox-mapper.rb
@@ -1,2 +1,2 @@
 # coding: utf-8
-require "ox/mapper"
+require 'ox/mapper'
--- lib/ox/mapper.rb
+++ lib/ox/mapper.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-require "ox/mapper/version"
+require 'ox/mapper/version'
 
 module Ox
   # Ox::Mapper's intention is to simplify usage of Ox::Sax parsers
@@ -7,15 +7,10 @@ module Ox
   #
   # Example:
   #   mapper = Ox::Mapper.new
-  #   mapper.on_element(:book) { |e| puts book.attributes.inspect }
-  #   mapper.on_element(:title) { |e| e.parent[:title] = e.text }
-  #
-  #   mapper.collect_attribute(:author => :name)
-  #   mapper.on_element(:author) { |e| e.parent[:author] = e[:name] }
-  #
-  #   # setup transformation for attribute "value" of "price" element
-  #   mapper.on_attribute(:price => :value) { |v| Float(v) }
-  #   mapper.on_element(:price) { |e| e.parent[:price] = e[:value] }
+  #   mapper.on(:book) { |e| puts book.attributes.inspect }
+  #   mapper.on(:title) { |e| e.parent[:title] = e.text }
+  #   mapper.on(:author, :attributes => :name) { |e| e.parent[:author] = e[:name] }
+  #   mapper.on_element(:price, :attributes => :value) { |e| e.parent[:price] = Float(e[:value]) }
   #
   #   mapper.parse(StringIO.new <<-XML) # => {:title => "Serenity", :author => "John Dow", :price => 1123.0}
   #     <xml>
@@ -27,7 +22,7 @@ module Ox
   #     </xml>
   #   XML
   module Mapper
-    autoload :Parser, "ox/mapper/parser"
+    autoload :Parser, 'ox/mapper/parser'
 
     def self.new
       Parser.new
